0fef36c814bf1c12ac4b609a640ae26153ff8cfc
Jake Chavez <jake94chavez@gmail.com>
2018-01-12 11:11:10 -0600

    server is up and running

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 /************
